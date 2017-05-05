0a979c6173f259afa215d5efbcf06e01b90dcd6f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-05-05 09:03:26 -0400

    added cyan version of angle2ybgr

--- colorspace/angle2ycgr.m
+++ colorspace/angle2ycgr.m
@@ -1,7 +1,7 @@
-function [ctab, value, rgb] = angle2ybgr(angle)
+function [ctab, value, rgb] = angle2ycgr(angle)
 % ANGLE2YBRG - Determine color by angle 0-360 degrees
 %
-% [CTAB, VALUE, RGB] = ANGLE2YBGR([ANGLE])
+% [CTAB, VALUE, RGB] = ANGLE2YCGR([ANGLE])
 %
 % Returns a color table for angle values 0:1:359. If a specific 
 % ANGLE is given as an input argument, the function will also return
@@ -9,16 +9,16 @@ function [ctab, value, rgb] = angle2ybgr(angle)
 % and the interpolated RGB value.
 %
 % The color space ranges from yellow ([1 1 0]) at 0 degrees, to green at
-% 90 degrees ([0 1 0]), to blue at 180 degrees ([0 0 1]), to red ([1 0 0]) 
+% 90 degrees ([0 1 0]), to cyan at 180 degrees ([0 1 1]), to red ([1 0 0]) 
 % at 270 degrees, and back to yellow at 360 degrees/0 degrees.
 %
-% See also: ANGLE2YCGR
+% See also: ANGLE2YBGR
 % 
 % Examples:
-%   ctab = angle2ybgr; % (can serve as a color map for angle data)
+%   ctab = angle2ycgr; % (can serve as a color map for angle data)
 %
 %   angle = 5.5;
-%   [ctab, value, rgb] = angle2ybgr(angle);
+%   [ctab, value, rgb] = angle2ycgr(angle);
 %   
 
 
