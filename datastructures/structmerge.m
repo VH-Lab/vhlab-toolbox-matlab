7731f1aab34388c76288899766534252b38570ea
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-06-13 08:54:46 -0400

    structmerge bug fix

--- datastructures/structmerge.m
+++ datastructures/structmerge.m
@@ -6,6 +6,7 @@ function s_out = structmerge(s1, s2, varargin)
 %  Merges the structures S1 and S2 into a common structure S_OUT
 %  such that S_OUT has all of the fields of S1 and S2. When 
 %  S1 and S2 share the same fieldname, the value of S2 is taken.
+%  The fieldnames will be re-ordered to be in alphabetical order.
 %
 %  The behavior of the function can be altered by passing additional
 %  arguments as name/value pairs. 
@@ -14,10 +15,12 @@ function s_out = structmerge(s1, s2, varargin)
 %  ------------------------------------------------------------
 %  ErrorIfNewField (0)     | (0/1) Is it an error if S2 contains a
 %                          |  field that is not present in S1?
+%  DoAlphabetical (1)      | (0/1) Alphabetize the field names in the result
 % 
 %  See also: STRUCT
 
 ErrorIfNewField = 0;
+DoAlphabetical = 1;
 
 assign(varargin{:});
 
@@ -31,9 +34,31 @@ if ErrorIfNewField,
 	end;
 end;
 
-s_out = s1;
+s_out_ = s1;
 
 for i=1:length(f2),
-	s_out = setfield(s_out,f2{i},getfield(s2,f2{i}));
+	if isempty(s_out_),
+		v = getfield(s2,f2{i});
+		eval(['s_out_(1).' f2{i} '=v;']);
+	else,
+		s_out_ = setfield(s_out_,f2{i},getfield(s2,f2{i}));
+	end;
 end;
+
+if DoAlphabetical,
+	fn = sort(fieldnames(s_out_));
+	s_out = emptystruct(fn{:});
+	for i=1:length(fn),
+		if isempty(s_out),
+			v = getfield(s_out_,fn{i});
+			eval(['s_out(1).' fn{i} '=v;']);
+		else,
+			s_out = setfield(s_out,fn{i},getfield(s_out_,fn{i}));
+		end;
+	end;
+else,
+	s_out = s_out_;
+end;
+
+
 
