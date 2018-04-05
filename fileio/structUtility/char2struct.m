d519584c420d48b7f6f677b9c03b909a549da645
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2018-04-05 10:51:26 -0400

    fix for char2struct

--- fileio/structUtility/char2struct.m
+++ fileio/structUtility/char2struct.m
@@ -7,7 +7,11 @@ pos = findstr(str,char(9));
 
 for i=1:length(fields)
 	t = str(pos(i)+1:pos(i+1)-1);
-	u = str2num(t);
+    if strcmp(t,'struct'), % we assume the user wants the string 'struct'
+        u = [];
+    else,
+    	u = str2num(t);
+    end
 	if ~isempty(u)
 		a = setfield(a,fields{i},u);
 	else
