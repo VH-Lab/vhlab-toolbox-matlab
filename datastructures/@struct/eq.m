c05b81a8d8c2c8c6c771588744112a8ac3b976a7
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2018-08-03 16:50:36 -0400

    structunique changed to equnique, more general scope

--- datastructures/@struct/eq.m
+++ datastructures/@struct/eq.m
@@ -1,21 +1,41 @@
 function b = eq(x,y)
+% EQ - are two structures equal?
+%
+% B = EQ(X, Y)
+%
+% Returns 1 if and only if the structures X and Y have the same
+% fieldnames and all fields have the same values.
+% 
 
 b=0;
+
 if isstruct(y),
- sz1 = size(x); sz2=size(y);
- if all(sz1==sz2),
-   b=1;
-   if prod(sz1)==1,
-       fn1=fieldnames(x); fn2=fieldnames(y);
-       if sort(fn1)==sort(fn2),
-          for i=1:length(fn1),
-            xv=getfield(x,fn1{i}); yv=getfield(y,fn1{i});
-	    if ~(eqlen(xv,yv)), b = 0; break; end;
-	  end;
-       else, b = 0;
-       end;
-   else,
-     for i=1:prod(sz1), if x(i)~=y(i), b = 0; break; end; end;
-   end;
- end;
+	sz1 = size(x);
+	sz2=size(y);
+	if all(sz1==sz2), % sizes match
+		b=1;
+		if prod(sz1)==1, % if we are down to single structures:
+			fn1=fieldnames(x);
+			fn2=fieldnames(y);
+			if sort(fn1)==sort(fn2), % don't require order to be ==
+				for i=1:length(fn1),
+					xv=getfield(x,fn1{i});
+					yv=getfield(y,fn1{i});
+					if ~(eqlen(xv,yv)),
+						b = 0; % we found a difference
+						break;
+					end;
+				end;
+			else,
+				b = 0;
+			end;
+		else, % call the function repeatedly on every x(i), y(i)
+			for i=1:prod(sz1),
+				if x(i)~=y(i),
+					b = 0;
+					break;
+				end;
+			end;
+		end;
+	end;
 end;
---
+++ datastructures/equnique.m
@@ -0,0 +1,37 @@
+function out = equnique(in)
+% EQUNIQUE - Return unique elements of an arbitrary class using EQ
+%
+% OUT = EQUNIQUE(IN)
+%
+% Returns the unique elements of an object array IN as a column OUT.
+% 
+% Uses EQ to test for equality.
+%
+% This function is a little more general than Matlab's UNIQUE in that it
+% can operate on any object that implements an EQ function, not just CELL arrays. 
+% It performs no sorting, and has no requirement that elements are ordered or sortable.
+%
+% Example:
+%    A=struct('A',5,'B',6);
+%    A = [A A A];
+%    B = equnique(A); % B == A(1)
+%
+% See also: UNIQUE
+
+n = numel(in);
+
+if n>=1,
+	out = in(1);
+else,
+	out = [];
+end
+
+tf=[];
+for i=1:n,
+	tf(i) = (in(i)~=out);
+end
+
+if any(tf),
+	out = cat(1,out,equnique(in(find(tf))));
+end
+
