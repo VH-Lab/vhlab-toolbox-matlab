60df22fbdffb4842a44593598e79d57f0ca15289
Steve Van Hooser <vanhoosr@brandeis.edu>
2018-08-26 10:19:42 -0400

    minor web page maker updates

--- html/facebook_htmltable.m
+++ html/facebook_htmltable.m
@@ -11,19 +11,29 @@ function str = facebook_htmltable(faceinfo, varargin)
 % ----------------------------------------------------------------
 % Ncols (3)                    | Number of columns
 % imagewidth (200)             | Image width, in pixels
-% 
+% nametablestylestring         |    Style of the name table entries 
+%   ('style="text-align: center; vertical-align:top; width:200px;font-size:100%;"')  | 
+% nametableprefix              | Prefix formatting for name table entries
+%  ('<font face="veranda, sans-serif"><b>')      | 
+% nametablepostfix             | Postfix formatting for name table entries
+%  ('</b></font>')             |
+% tableidstr ('')              | If using a style, could be ' id="facebooktable"' for example
 %
 % See also: CELLSTR2TEXT, NAMEVALUEPAIR
 %
 
 image_width = 200;
 Ncols = 3;
+nametablestylestring = 'style="text-align: center; vertical-align: top; width:200px;font-size:100%;"';
+nametableprefix = '<font face="veranda, sans-serif"><b>';
+nametablepostfix = '</b></font>';
+tableidstr= '';
 
 assign(varargin{:});
 
 str = {};
 
-str{end+1} = ['<table id="facebooktable">'];
+str{end+1} = ['<table ' tableidstr '>'];
 
 Nrows = ceil(numel(faceinfo)/3);
 
@@ -32,15 +42,18 @@ for n=1:Nrows,
 
 	for r=1:2,
 		str{end+1} = ['   <tr>'];
-		for c = 1:3,
-			entry = (n-1)*3+c;
+		for c = 1:Ncols,
+			entry = (n-1)*Ncols+c;
 			if numel(faceinfo) >= entry,
-				str{end+1} = ['      <td>'];
+				str{end+1} = ['      <td ' nametablestylestring '>'];
 
 				if r==1,
 					str{end+1} = ['      <img src="' faceinfo(entry).imagefile '" alt="' faceinfo(entry).name '" width=' int2str(image_width) 'px>' ];
 				elseif r==2,
-					str{end+1} = ['      ' faceinfo(entry).name ];
+					str{end+1} = ['      ' ...
+						nametableprefix ...
+						faceinfo(entry).name ...
+						nametablepostfix  ];
 				end
 
 				str{end+1} = ['      </td>'];
