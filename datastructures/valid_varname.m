065769443335f112564c63b46a98d0e7c6bb1244
Steve Van Hooser <vanhoosr@brandeis.edu>
2018-09-09 13:14:41 -0400

    celltable2struct added

--- datastructures/valid_varname.m
+++ datastructures/valid_varname.m
@@ -9,7 +9,7 @@ function b = validvarname(varname)
 %
 %  Note: There is now a Matlab function that does this: ISVARNAME
 %
-%  See also: ISVARNAME
+%  See also: ISVARNAME, MATLAB.LANG.MAKEVALIDNAME
 
 b=1;
 try, eval([varname '=5;']); catch, b=0; end;
--- file/read_tab_delimited_file.m
+++ file/read_tab_delimited_file.m
@@ -1,16 +1,18 @@
-function out = readtabdelimitedfile(fname)
+function out = read_tab_delimited_file(fname)
 
-% READTABDELIMITEDFILE - Reads data from a tab-delimited file; tries to sort string/number data
+% READ_TAB_DELIMITED_FILE - Reads data from a tab-delimited file; tries to sort string/number data
 %
 %
-%   OUTPUT = READTABDELIMTEDFILE(FILENAME)
+%   OUTPUT = READ_TAB_DELIMTED_FILE(FILENAME)
 %
 %   Reads data from a tab-delimited text file.  OUTPUT is a cell list.  OUTPUT{i,j} is
 %   the value from the ith row and jth column in the tab-delimited file; note that the
 %   rows and columns need not have the same number of entries.
 %
-%   This function also tries to assign each value a number using the function STR2NUM.
-%   If this process fails, then the value is assumed to be a text string and is stored as such.
+%   The function looks at each string. If it has two '/' anywhere in them, the value is assumed
+%   to be a date and is read as a string. Otherwise, this function tries to assign each value a
+%   number using the function STR2NUM. If this process fails, then the value is assumed to be a
+%   text string and is stored as such.
 %
 
 fid = fopen(fname,'rt');
@@ -29,12 +31,13 @@ while mynextline~=-1, % this will be -1 if fgetl can't read anything else
 		myseparator = [1 myseparator length(mynextline)]; % pretend the first character and last character are separators
 		for j=2:length(myseparator), % read all the info between the separators
 			value = mynextline(myseparator(j-1):myseparator(j));
-			value_number = str2num(value);
-			if isempty(value_number),
-				out{linenumber}{j-1} = value;
-			else,
-				out{linenumber}{j-1} = value_number;
-			end;
+			out{linenumber}{j-1} = value;
+			if numel(find(value=='/')) < 2,
+				value_number = str2num(value);
+				if ~isempty(value_number),
+					out{linenumber}{j-1} = value_number;
+				end;
+			end
 		end;
 	end;
 	linenumber = linenumber + 1;
