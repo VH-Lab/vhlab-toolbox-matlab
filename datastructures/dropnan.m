527fca6e0790f5371af9d002ebaf22a7a465c86c
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2018-09-24 11:58:44 -0400

    added dropnan

---
+++ datastructures/dropnan.m
@@ -0,0 +1,16 @@
+function b = dropnan(a)
+% DROPNAN - return a vector with NaN entries dropped
+%
+% B = DROPNAN(A)
+%
+% Given a vector A, this function will return a vector B, which will have
+% all non-NaN entries of A but will exclude NaN entries.
+%
+
+if min(size(A))~=1,
+	error(['This function requires a vector as input.']);
+end
+
+g = ~isnan(a);
+b = a(g);
+
