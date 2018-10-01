6cd8959cfaf2bde23d2a6f14fa65309af486c717
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2018-10-01 07:13:11 -0400

    small updates

--- neuroscience/membrane_voltage/mle_normal_vm_fr.m
+++ neuroscience/membrane_voltage/mle_normal_vm_fr.m
@@ -1,18 +1,18 @@
-function [params] = mle_poisson_vm_fr(vm, fr, deltaT, varargin)
-% MLE_POISSON_VM_FR - use maximum likelihood analysis to compute Poisson rate model of voltage -> firing rate
+function [params] = mle_normal_vm_fr(vm, fr, deltaT, varargin)
+% MLE_NORMAL_VM_FR - use maximum likelihood analysis to compute Poisson rate model of voltage -> firing rate
 % 
-% PARAMS = MLE_POISSON_VM_FR(VM, FR, DELTAT,  ...)
+% PARAMS = MLE_NORMAL_VM_FR(VM, FR, DELTAT,  ...)
 %
-% Estimate the parameters of a Poisson rate function for fitting spike counts in bins and
+% Estimate the parameters of a Normal rate function for fitting spike counts in bins and
 % voltage values in bins.
 %
 % VM should be measurements of membrane potential in bins of size DELTAT
 % FR should be measurements of firing rate (N_SPIKES/DELTAT)
 %
 % It returns the free PARAMS for each fit function type.
-% For 'linearthreshold', PARAMS is [slope threshold] (see help LINEPOWERTHRESHOLD)
-% For 'linepowerthreshold', PARAMS is [slope threshold exponent]
-% For 'Poisson', PARAMS is [rate]
+% For 'linearthreshold', PARAMS is [slope threshold sigma] (see help LINEPOWERTHRESHOLD)
+% For 'linepowerthreshold', PARAMS is [slope threshold exponent sigma]
+% For 'Normal', PARAMS is [rate sigma]
 %
 % This function also accepts name/value pairs that alter its behavior:
 % Parameters (default)              | Description
@@ -32,10 +32,9 @@ function [params] = mle_poisson_vm_fr(vm, fr, deltaT, varargin)
 %
 % Example: (simple test, without voltage dependence)
 %   deltaT = 0.030; % 30 ms bins
-%   n = icdf('Poisson',rand(500,1),2); % generate spike counts in bins, rate of 2
-%   fr = n / deltaT; % convert to rates
-%   p_out =mle_poisson_vm_fr(0*fr,fr,deltaT,'fit_function','Poisson')  
-%   p_out_matlab = mle(round(fr*deltaT),'distribution','Poisson' ) / deltaT % they match
+%   fr = icdf('Normal',rand(500,1),2,1); % generate rates, rate of 2, standard deviation of 1
+%   p_out =mle_normal_vm_fr(0*fr,fr,deltaT,'fit_function','Normal')  
+%   p_out_matlab = mle(fr,'distribution','Normal' ) % they match
 
 fit_function = 'linearthreshold';
 nspace = 5;
@@ -43,21 +42,21 @@ nspace = 5;
 assign(varargin{:});
 
 switch lower(fit_function),
-	case 'poisson',
-		lower_bounds = 0;
-		upper_bounds = [10*max(fr)];
+	case 'normal',
+		lower_bounds = [0 min(0.1/deltaT,0.1*std(fr))];
+		upper_bounds = [10*max(fr) 10/deltaT];
 		rateFunc = 'linepowerthreshold';
-		fitfun = @(x) -poisson_vm_fr(vm,fr,deltaT,rateFunc, {1 x(1) 0 1});
+		fitfun = @(x) -normal_vm_fr(vm,fr,deltaT,rateFunc, {1 x(1) 0 1}, x(2));
 	case 'linearthreshold',
-		lower_bounds = [0 0];
-		upper_bounds = [10*max(fr)/max(vm) max(vm)] ;
+		lower_bounds = [0 0 min(0.1/deltaT,0.1*std(fr))];
+		upper_bounds = [10*max(fr)/max(vm) max(vm) 10/deltaT] ;
 		rateFunc = 'linepowerthreshold';
-		fitfun = @(x) -poisson_vm_fr(vm,fr,deltaT,rateFunc, {x(1) eps x(2) 1});
+		fitfun = @(x) -normal_vm_fr(vm,fr,deltaT,rateFunc, {x(1) eps x(2) 1}, x(3));
 	case 'linepowerthreshold',
-		lower_bounds = [0 0 0.5];
-		upper_bounds = [10*max(fr)/max(vm) max(vm) 5] ;
+		lower_bounds = [0 0 0.5 min(0.1/deltaT,0.1*std(fr))];
+		upper_bounds = [10*max(fr)/max(vm) max(vm) 5 10/deltaT] ;
 		rateFunc = 'linepowerthreshold';
-		fitfun = @(x) -poisson_vm_fr(vm,fr,deltaT,rateFunc, {x(1) eps x(2) x(3)});
+		fitfun = @(x) -normal_vm_fr(vm,fr,deltaT,rateFunc, {x(1) eps x(2) x(3)}, x(4));
 	otherwise,
 		error(['Unknown fit function']);
 end;
