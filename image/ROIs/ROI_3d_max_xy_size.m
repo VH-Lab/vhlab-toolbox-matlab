56e64ab5d15876120ebcf27c5673f7c376eda624
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-07-03 12:55:03 -0400

    add progress bar to ROI_3d_max_xy_size

--- image/ROIs/ROI_3d_max_xy_size.m
+++ image/ROIs/ROI_3d_max_xy_size.m
@@ -1,13 +1,18 @@
-function max_xy_size = ROI_3d_max_xy_size(CC)
+function max_xy_size = ROI_3d_max_xy_size(CC, varargin)
 % ROI_3D_MAX_XY_SIZE - compute the maximum X/Y size of a 3-d ROI
 %
-% MAX_XY_SIZE = ROI_3D_MAX_XY_SIZE(CC)
+% MAX_XY_SIZE = ROI_3D_MAX_XY_SIZE(CC, ...)
 %
 % Given a structure of regions-of-interest such as that returned by
 % BWCONNCOMP, with fields 'ImageSize' and 'PixelIdxList', this function
 % examines all Z planes and computes the area (in pixels) of each
 % ROI in X and Y (dimensions 1 and 2). The maximum such value is returned
 % in MAX_XY_SIZE(i), for each ROI in CC (i goes from 1 .. numel(CC.PixelIdxList)).
+%
+% This function also takes NAME/VALUE pairs that modify the behavior:
+% Parameter (default)           | Description
+% --------------------------------------------------------------------
+% UseProgressBar (1)            | Should we use a progress bar? (0/1)
 %
 % Example:
 %     % make a simple image
@@ -21,13 +26,27 @@ function max_xy_size = ROI_3d_max_xy_size(CC)
 %     max_xy_size = ROI_3d_max_xy_size(CC)
 %
 
+UseProgressBar = 1;
+
+assign(varargin{:});
+
 max_xy_size = -Inf * ones(1,numel(CC.PixelIdxList));
+
+if UseProgressBar,
+	progressbar('Max XY calculation progress');
+end;
 
 for z=1:CC.ImageSize(3),
 	for j=1:numel(CC.PixelIdxList),
 		indexes_here = ROI_3d2dprojection(CC.PixelIdxList{j}, CC.ImageSize, z);
 		max_xy_size(j) = max( max_xy_size(j), numel(indexes_here) );
+		if UseProgressBar & ( (j==1) | mod(j,100)==0 ) ,
+			progressbar( (j+(z-1)*numel(CC.PixelIdxList))/(numel(CC.PixelIdxList)*CC.ImageSize(3)) );
+		end;
 	end;
 end;
 
+if UseProgressBar,
+	progressbar(1); % ensure it is closed 
+end;
 
