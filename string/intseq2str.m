a566d298d2bfc7c6086689d6ce4c6d881e9c389f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-07-15 15:48:46 -0400

    embarrassing bug in intseq2str, fixed

--- string/intseq2str.m
+++ string/intseq2str.m
@@ -29,19 +29,20 @@ if isempty(A),
 	return;
 end;
 
-d = [Inf diff(A)];
+d = [Inf diff(A) Inf];
 d_points = find(d~=1);
 
-for i=1:numel(d_points),
+for i=1:numel(d_points)-1,
+    % we are at the beginning of a run that may last 1 or more entries
 	str = [str int2str(A(d_points(i)))];
-	if (i+1)<=length(d_points),
+	if (i+1)<=length(d_points), % is there another value left?
 		gap = d_points(i+1)-d_points(i);
 		if gap>1, % complete the sequence
 			str = [str seq int2str(A(d_points(i+1)-1))];
 			if (i+2)<=length(d_points), % need to add separator
 				str = [str sep];
 			end;
-		else,  % there's another point but it's not sequential
+		elseif i~=numel(d_points)-1,  % there's another point but it's not sequential
 			str = [str sep];
 		end;
 	end;
