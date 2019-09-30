c23c97ceef08d273d4b7f4fa311075af9735aa2f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-09-30 14:21:29 -0400

    added rowvec

--- datastructures/colvec.m
+++ datastructures/colvec.m
@@ -12,6 +12,8 @@ function y = colvec(x)
 %  This function is useful for addressing all of the elements of a matrix
 %  as a vector when X is a subset of another matrix.
 %
+%  See also: ROWVEC
+%
 %  Example: 
 %    A = rand(5,5)
 %    Y = colvec(A(1:3,1:2)) % returns points in rows 1:3, columns 1:2
