bef09c179158bc4c49d62c6382b496c597d47f44
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2019-10-09 12:09:58 -0400

    matrow2cell.m

---
+++ datastructures/matrow2cell.m
@@ -0,0 +1,24 @@
+function b = matrow2cell(a)
+% MATROW2CELL - Convert a matrix to a cell array with each row as a cell
+% 
+% B = MATROW2CELL(A)
+%
+% Given a matrix A, a cell array B is created such that the entries of 
+% B{i} are the ith rows of A.
+%
+% If A is a cell array, then no action is taken and B = A.
+%
+% One might want to use this when using JSONDECODE, when regularly-sized
+% items might be returned as a matrix instead of the more general cell array.
+% 
+
+if iscell(a),
+	b = a;
+	return;
+end;
+
+b = {};
+for i=1:size(a,1),
+	b{i} = a(i,:);
+end;
+
