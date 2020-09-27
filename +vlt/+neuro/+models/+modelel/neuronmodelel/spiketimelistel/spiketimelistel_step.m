cddab71883879377a9a3390e2e99c539abcb0511
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-09-27 09:03:57 -0400

    fixed reverse correlation directories

--- +vlt/+neuro/+models/+modelel/neuronmodelel/spiketimelistel/spiketimelistel_step.m
+++
@@ -1,9 +0,0 @@
-function spiketimelistel = spiketimelistel_step(spiketimelistel,modelstruct)
-% SPIKETIMELISTEL_STEP - Performs no operation
-%
-%   SPIKETIMELISTEL = vlt.neuro.models.modelel.neuronmodelel.spiketimelistel.spiketimelistel_step(SPIKETIMELISTEL, MODELSTRUCT)
-%
-%   Just adds dT T.
-
-spiketimelistel.T = spiketimelistel.T + spiketimelistel.dT;
-
