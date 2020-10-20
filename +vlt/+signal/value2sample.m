707d23d79c2bff3ac638565371c4e327e35868b1
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2020-10-20 13:50:16 -0400

    added value2sample, sample2value

---
+++ +vlt/+signal/value2sample.m
@@ -0,0 +1,17 @@
+function s = value2sample(v, sr, v1)
+% vlt.signal.value2sample - give the sample number corresponding to a quantity that is sampled at a fixed rate (value2sample)
+%
+% S = VALUE2SAMPLE(V, SR, V1)
+%
+% Given a value (or array of values) that are sampled at a fixed sampling
+% rate SR (in samples/sec), and given the value of sample number 1, 
+% returns the sample numbers.
+%
+% See also: vlt.signal.sample2value
+% 
+% Example:
+%    s = vlt.signal.value2sample(1, 1000, 0) % s is 1001
+%
+
+s = 1+sr*(v-v1);
+
