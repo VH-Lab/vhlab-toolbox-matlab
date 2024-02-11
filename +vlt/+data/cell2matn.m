6cf0d8e5991f268728320c86193c7aa806ed4879
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-02-11 10:47:49 -0500

    cell2matn function added

---
+++ +vlt/+data/cell2matn.m
@@ -0,0 +1,26 @@
+function m = cell2matn(c)
+% CELL2MATN - convert a cell matrix to a numeric matrix with NaN for empty
+%
+% M = CELL2MATN(C)
+%
+% Create a numeric matrix from a cell matrix C. Empty entries of C will be
+% coded as NaN entries in M. C must contain only numeric entries.
+% 
+% Example:
+%   a{1,1} = 1; a{1,2} = 2; a{2,1} = 3;
+%   m = vlt.data.cell2matn(a);
+%   % m is [ 1 2 ; 3 NaN]
+%
+% See also: CELL2MAT, MAT2CELL
+
+m = NaN * ones(size(c));
+
+for i=1:numel(c),
+	v = c{i};
+	if ~isempty(v),
+		if ~isnumeric(v),
+			error(['Non-numeric entry encountered in C.']);
+		end;
+		m(i) = v;
+	end;
+end;
