d029af90e12e212a377c3af8034aed59fc195499
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-03-20 10:30:41 -0400

    fix for relativeFilename, now adds .. as needed

--- +vlt/+file/relativeFilename.m
+++ +vlt/+file/relativeFilename.m
@@ -12,25 +12,43 @@ function relFname = relativeFilename(abs_path, abs_filename, file_separator)
 % If FILE_SEPARATOR isn't specified, it is taken to be FILESEP,
 % the file separator on the platform currently running Matlab.
 %
-% Example:
+% Examples:
 %    abs_path = '/Users/myusername/Documents/';
 %    abs_filename = '/Users/myusername/Documents/myfolder/myfile.txt'
 %    relFname = vlt.file.relativeFilename(abs_path, abs_filename)
-%    % relFname == myfolder/myfile.txt'
+%    % relFname = 'myfolder/myfile.txt'
+%    abs_path = '/Users/myusername/Documents/';
+%    abs_filename = '/Users/myusername/myfolder/myfile.txt'
+%    relFname = vlt.file.relativeFilename(abs_path, abs_filename)
+%    % relFname = '../myfolder/myfile.txt'
 % 
 
 if nargin<3,
 	file_separator = filesep;
 end;
 
+if vlt.file.isurl(abs_filename),
+	error(['Does not work with URLs. URLs are absolute paths.']);
+end;
+
 abs_path = vlt.file.tidypathstring(abs_path, file_separator);
 abs_path(end+1) = file_separator;
 abs_filename = vlt.file.tidypathstring(abs_filename, file_separator);
 
-f = findstr(abs_filename,abs_path);
-
-if ~isempty(f),
-	relFname = abs_filename(f(1)+numel(abs_path):end);
-else,
-	error(['Could find no relative path for ' abs_filename ' at path ' abs_path '.']);
+index_difference = [];
+for i=1:numel(abs_path),
+	if abs_path(i)~=abs_filename(i),
+		index_difference = i
+		break;
+	end;
 end;
+
+if isempty(index_difference), % then we just take what is left
+	relFname = abs_filename(numel(abs_path)+1:end);
+else,
+	% paths diverged earlier
+	num_dots = numel(find(file_separator==abs_path(index_difference:end)));
+	relFname = [repmat(['..' file_separator],1,num_dots) abs_filename(index_difference:end)];
+end;
+
+
